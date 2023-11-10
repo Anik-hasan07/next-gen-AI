be2d8d6bcf7ab36e251d5fcdf2225176698a4873
Anik-hasan07 <iamakashcse@gmail.com>
2023-11-10 15:24:24 +0600

    feature/main

---
+++ app/(Main)/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import Header from '@/components/Header'
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div>
+        <Header/>
+    </div>
+  )
+}
+
+export default page
